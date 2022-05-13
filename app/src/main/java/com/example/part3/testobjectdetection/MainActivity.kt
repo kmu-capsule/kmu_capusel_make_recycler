79e5f12b38041a53556fc42119a19d99099ec3cf
‍김호준(학부생-소프트웨어전공) <hjk6322@kookmin.ac.kr>
2022-05-13 16:41:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/part3/testobjectdetection/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.part3.testobjectdetection
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
