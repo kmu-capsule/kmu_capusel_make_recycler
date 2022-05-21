9d6a155ce91d57990c41c473d2b49ec7835824fb
dan-2ee <ekdms7273@naver.com>
2022-05-22 00:51:28 +0900

    feat: Firebase storage image 저장

--- app/src/main/java/com/example/part3/testobjectdetection/MainActivity.kt
+++ app/src/main/java/com/example/part3/testobjectdetection/MainActivity.kt
@@ -3,18 +3,30 @@ package com.example.part3.testobjectdetection
 import android.content.Intent
 import android.database.Cursor
 import android.graphics.BitmapFactory
+import android.media.Image
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
+import com.google.firebase.storage.FirebaseStorage
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var ImageURI: Uri     // 이미지 uri
+    private var ImageAbsPath: String? = ""    // uri로부터 얻어온 이미지 절대 경로
     private val GALLERY_CODE: Int = 1112
+
     private val ImageSelect: ImageButton by lazy {
         findViewById(R.id.ImageSelect)
+    }
+    private val ImageUpload: Button by lazy {
+        findViewById(R.id.ImageUpload)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +35,10 @@ class MainActivity : AppCompatActivity() {
 
         ImageSelect.setOnClickListener {
             openGallery()
+        }
+
+        ImageUpload.setOnClickListener {
+            ImageUploadToStorage()
         }
     }
 
@@ -39,12 +55,20 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
+            ImageURI = data?.data.toString().toUri()
             when (requestCode) {
-                //갤러리에서 가져오기
-                GALLERY_CODE -> data?.data?.let { getRealPathFromURI(it) }
-                    ?.let { Log.i("image", it) }   // 이미지 경로 로그에 출력
+                GALLERY_CODE ->
+                    ImageAbsPath = data?.data?.let { getRealPathFromURI(it) }
+//                GALLERY_CODE -> data?.data?.let { getRealPathFromURI(it)?.let { Log.i("imagePath", it) } }
                 else -> {}
             }
+            // 이미지 Uri 로그에 출력
+            Log.i("ImageUri", ImageURI.toString())
+            // 선택된 이미지 화면에 띄워줌
+            ImageSelect.setImageURI(ImageURI)
+            Toast.makeText(baseContext, "사진 선택 완료", Toast.LENGTH_SHORT).show()
+            // 이미지 절대 경로 로그에 출력
+            Log.i("ImagePath", ImageAbsPath.toString())
         }
     }
 
@@ -54,6 +78,20 @@ class MainActivity : AppCompatActivity() {
         intent.setType("image/*");
         // 갤러리 인텐트를 부르고 사진을 선택하면 이전 액티비티로 돌아옴
         startActivityForResult(intent, GALLERY_CODE);
+    }
+
+    private fun ImageUploadToStorage() {
+        // FirebaseStorage 인스턴스 생성
+        var fbStorage: FirebaseStorage? = FirebaseStorage.getInstance()
+        // FileName 생성
+        val fileName = "${System.currentTimeMillis()}.png"
+        Log.i("FileName", fileName)
+
+        var storageRef = fbStorage?.reference?.child("object/photo")?.child(fileName)
+
+        storageRef?.putFile(ImageURI!!)?.addOnSuccessListener {
+            Toast.makeText(baseContext, "이미지 업로드 완료", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
